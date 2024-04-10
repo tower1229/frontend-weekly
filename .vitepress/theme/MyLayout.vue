5a608c1c0648758db10120e935c2efad7ef4a3a8
refined-x <tower1229@foxmail.com>
2024-04-10 13:35:51 +0800

    feat: add baidu

---
+++ .vitepress/theme/MyLayout.vue
@@ -0,0 +1,19 @@
+<!--.vitepress/theme/MyLayout.vue-->
+<script setup>
+import DefaultTheme from "vitepress/theme";
+
+const { Layout } = DefaultTheme;
+</script>
+<script>
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2de20984d875f33eca1256139bdead79";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
+<template>
+  <Layout> </Layout>
+</template>
